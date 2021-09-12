c101a93a3662f25aa3087348e798ae812c59abeb
kakeruAoyama <64327383+kakeruAoyama@users.noreply.github.com>
2021-09-12 22:32:08 +0900

    Update lecture.rb
    usersの違いは付ける

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -4,8 +4,8 @@ class Lecture < ApplicationRecord
     has_many :evaluations
 
     # 多対多のアソシエーション
-    has_many :users, through: :current_lectures
-    has_many :users, through: :past_lectures
+    has_many :current_users, through: :current_lectures
+    has_many :past_users, through: :past_lectures
     has_many :past_lectures
     has_many :current_lectures
 end
